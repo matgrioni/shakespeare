8fd7b444d9c4cea0f521fdecc8d36305589f2850
Matias Grioni <matgrioni@gmail.com>
2017-04-08 16:53:24 -0400

    Randomly assign non hostile dyads by sampling 2 characters at a time

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import itertools
 import re
 import sys
 
+import numpy
 from pycorenlp import StanfordCoreNLP
 
 import analysis
@@ -51,63 +52,86 @@ if len(sys.argv) >= 3:
 else:
     raise TypeError('Illegal number of arguments.')
 
+p = Play(play_filename)
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 # The annotation key is the file that explicitly states which annotation ids
-# are associated with which characters.
-annotations = {}
+# are associated with which characters. These are all the hostile dyads that
+# will be tracked in the play.
+hostile_dyads = {}
 with open(ann_key_filename, 'r') as f:
     for line in f:
         items = line.rstrip().split(', ')
-        annotations[tuple(items[1:3])] = (items[0], items[3])
+        hostile_dyads[tuple(items[1:3])] = (items[0], items[3])
 
-p = Play(play_filename)
+# Now we have to find out all the non-hostile dyads and arbitrairly assign a
+# betrayer and victim to them.
+# TODO: Maybe use frozenset instead.
+names = map(lambda c: c.short or c.name, p.characters)
+non_hostile_dyads = []
+while len(names) > 1:
+    choices = tuple(numpy.random.choice(names, size=2))
+    rev_choices = (choices[1], choices[0])
+
+    while choices in hostile_dyads or rev_choices in hostile_dyads:
+        choices = numpy.random.choice(names, size=2)
+
+    names.remove(choices[0])
+    names.remove(choices[1])
+
+    non_hostile_dyads.append(choices)
+
+print hostile_dyads
+print non_hostile_dyads
 
 # Each annotation consists of the key value in the text and also a dyad or pair
 # of characters involved in the betrayal.
 betrayer_lines = []
 victim_lines = []
-non_hostile_lines = []
-
-names = map(lambda c: c.short or c.name, p.characters)
-current_dyads = dict.fromkeys(itertools.permutations(names, 2))
+arb_betrayer_lines = []
+arb_victim_lines = []
 
 for atom in p.atoms:
     hostile_pair_found = False
     valid_line = True
 
-    for dyad, _ in current_dyads.items():
-        try:
-            info = annotations[dyad]
+    for dyad, info in hostile_dyads.items():
+        if info:
             m = re.search(info[0] + '_HOSTILE_' + info[1]  + '_BEGIN', atom.content)
             if m:
-                del current_dyads[dyad]
+                del hostile_dyads[dyad]
                 continue
 
             try:
                 if dyad[0] == atom.speaker and dyad[1] in atom.audience:
                     betrayer_lines.append(atom.content)
                     hostile_pair_found = True
-                if dyad[1] == atom.speaker and dyad[0] in atom.audience:
+                elif dyad[1] == atom.speaker and dyad[0] in atom.audience:
                     victim_lines.append(atom.content)
                     hostile_pair_found = True
             except AttributeError:
                 # This line is not a Line instance, but an Annotation or
                 # StageNote so we can ignore it.
                 valid_line = False
-        except KeyError:
-            pass
 
     if not hostile_pair_found and valid_line:
-        non_hostile_lines.append(atom.content)
+        for dyad in non_hostile_dyads:
+            betrayer = dyad[0]
+            victim = dyad[1]
+            if betrayer == atom.speaker and victim in atom.audience:
+                arb_betrayer_lines.append(atom.content)
+            elif victim == atom.speaker and betrayer in atom.audience:
+                arb_victim_lines.append(atom.content)
 
 betrayer_diag = ' '.join(betrayer_lines)
 victim_diag = ' '.join(victim_lines)
-non_hostile_diag = ' '.join(non_hostile_lines)
+arb_betrayer_diag = ' '.join(arb_betrayer_lines)
+arb_victim_diag = ' '.join(arb_victim_lines)
 
 betrayer_sents = analysis.sentiment(nlp, betrayer_diag)
 victim_sents = analysis.sentiment(nlp, victim_diag)
-non_hostile_sents = analysis.sentiment(nlp, non_hostile_diag, 17000)
+arb_betrayer_sents = analysis.sentiment(nlp, arb_betrayer_diag, 17000)
+arb_victim_sents = analysis.sentiment(nlp, arb_victim_diag, 17000)
 
 if len(betrayer_sents) > 0:
     p_b = sentiments_to_percent_positive(betrayer_sents)
@@ -127,16 +151,26 @@ else:
     p_v = 0
     bootstrap_v = (0, 0, 0)
 
-if len(non_hostile_sents) > 0:
-    p_nh = sentiments_to_percent_positive(non_hostile_sents)
-    bootstrap_nh = analysis.bootstrap(non_hostile_sents,
-                                      sentiments_to_percent_positive,
-                                      BOOTSTRAP_NUM_SAMPLES)
+if len(arb_betrayer_sents) > 0:
+    p_ab = sentiments_to_percent_positive(arb_betrayer_sents)
+    bootstrap_ab = analysis.bootstrap(arb_betrayer_sents,
+                                     sentiments_to_percent_positive,
+                                     BOOTSTRAP_NUM_SAMPLES)
 else:
-    p_nh = 0
-    bootstrap_nh = (0, 0, 0)
+    p_ab = 0
+    bootstrap_ab = (0, 0, 0)
+
+if len(arb_victim_sents) > 0:
+    p_av = sentiments_to_percent_positive(arb_victim_sents)
+    bootstrap_av = analysis.bootstrap(arb_victim_sents,
+                                     sentiments_to_percent_positive,
+                                     BOOTSTRAP_NUM_SAMPLES)
+else:
+    p_av = 0
+    bootstrap_av = (0, 0, 0)
 
 # Print out the results.
+print 'Hostile dyad analysis'
 print 'Betrayer sentences: ' + str(len(betrayer_sents))
 print 'Betrayer words: ' + str(len(betrayer_diag.split(' ')))
 print 'Betrayer percent of sentences positive: ' + str(p_b)
@@ -147,8 +181,15 @@ print 'Victim words: ' + str(len(victim_diag.split(' ')))
 print 'Victim percent of sentences positive: ' + str(p_v)
 print 'Victim bootstrapped CI (95%): ' + str(bootstrap_v[0:2])
 print 'Victim bootstrapped SE: ' + str(bootstrap_v[2])
-print 'Non hostile sentences: ' + str(len(non_hostile_sents))
-print 'Non hostile words: ' + str(len(non_hostile_diag.split(' ')))
-print 'Non hostile percent of sentences positive: ' + str(p_nh)
-print 'Non hostile bootstrapped CI (95%): ' + str(bootstrap_nh[0:2])
-print 'Non hostile bootstrapped SE: ' + str(bootstrap_nh[2])
+print
+print 'Non hostile dyad analysis'
+print 'Betrayer sentences: ' + str(len(arb_betrayer_sents))
+print 'Betrayer words: ' + str(len(arb_betrayer_diag.split(' ')))
+print 'Betrayer percent of sentences positive: ' + str(p_ab)
+print 'Betrayer bootstrapped CI (95%): ' + str(bootstrap_ab[0:2])
+print 'Betrayer bootstrapped SE: ' + str(bootstrap_ab[2])
+print 'Victim sentences: ' + str(len(arb_victim_sents))
+print 'Victim words: ' + str(len(arb_victim_diag.split(' ')))
+print 'Victim percent of sentences positive: ' + str(p_av)
+print 'Victim bootstrapped CI (95%): ' + str(bootstrap_av[0:2])
+print 'Victim bootstrapped SE: ' + str(bootstrap_av[2])
